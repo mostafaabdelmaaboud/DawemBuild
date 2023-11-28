002435f04b4e7d4372a1641e825c3555a387c881
mostafa <mostafaabdelmaaboud3@gmail.com>
2023-11-29 00:42:51 +0200

    build v1

---
+++ 208.429cc6dc32269c93.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkDawem=self.webpackChunkDawem||[]).push([[208],{3208:(M,t,a)=>{a.r(t),a.d(t,{TablesModule:()=>u});var l=a(6895),E=a(6911),n=a(5412),o=a(7590),_=a(4650);let u=(()=>{class s{static#a=this.\u0275fac=function(D){return new(D||s)};static#s=this.\u0275mod=_.oAB({type:s});static#t=this.\u0275inj=_.cJS({imports:[l.ez,E.R,n.Is,o.m]})}return s})()}}]);
